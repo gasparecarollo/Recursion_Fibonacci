c2e4460c0fa8a0eef7e86f2e3fa4d123f21974b1
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-15 14:05:25 -0500

    Adding the Iterative function

--- Recursion.js
+++ Recursion.js
@@ -3,3 +3,6 @@
 //A function that calls itself is called recursiveFibSequence
 //A recursive function call itself to reduce a larger problem into a smaller one until it can be solved.
 
+//A Recursive function must have two properties:
+// -A simple base case (or cases) - which is a terminating scenario that does not use recursion to produce an answer
+// -The inductive step - A set of rules that reduce all other cases toward the base case
---
+++ iterative.js
@@ -0,0 +1,9 @@
+//Write a function that sums all of the numbers up to and including a given number
+const addition = (num) => {
+
+
+
+
+}
+
+console.log(addition(10))
