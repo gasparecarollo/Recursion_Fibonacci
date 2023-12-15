2b8b5a7eebf87dfb639e6d20e0ac84f3fff58b25
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-15 13:29:59 -0500

    Defined Recursion

--- Recursion.js
+++ Recursion.js
@@ -1,2 +1,5 @@
 //Recursion
 
+//A function that calls itself is called recursiveFibSequence
+//A recursive function call itself to reduce a larger problem into a smaller one until it can be solved.
+
