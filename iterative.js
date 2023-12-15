bccfa2c45cc0e70786f4832442b02a8730007e58
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-15 14:12:13 -0500

    Created Iterative function for adding upwards

--- iterative.js
+++ iterative.js
@@ -1,9 +1,16 @@
 //Write a function that sums all of the numbers up to and including a given number
 const addition = (num) => {
+    //Initialize a total var
+    let total = 0
 
+    //iterate up to/ or down from the number
+    for (let i = 0; i <= num; i + 1) {
+        //add each number to our total
+        total += i
+    }
 
-
-
+    //return total variable
+    return total
 }
 
 console.log(addition(10))
