5a56a1925948f0c2d96c48880598f6c02f15aeea
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-12-18 14:39:59 -0500

    Recursion

--- Recursion.js
+++ Recursion.js
@@ -6,3 +6,15 @@
 //A Recursive function must have two properties:
 // -A simple base case (or cases) - which is a terminating scenario that does not use recursion to produce an answer
 // -The inductive step - A set of rules that reduce all other cases toward the base case
+
+const recAddition = (num) => {
+    //Write your base case
+    if (num === 1) {
+        return 1
+    }
+    //Recursive Step
+    return num + recAddition(num - 1)
+}
+console.log(recAddition(1)) // => Base case 1
+console.log(recAddition(2)) // => 3
+console.log(recAddition(3))// => 6
--- iterative.js
+++ iterative.js
@@ -3,7 +3,7 @@ const addition = (num) => {
     //Initialize a total var
     let total = 0
 
-    //iterate up to/ or down from the number
+    //iterate UP from the number
     for (let i = 0; i <= num; i + 1) {
         //add each number to our total
         total += i
@@ -12,5 +12,15 @@ const addition = (num) => {
     //return total variable
     return total
 }
+
+console.log(addition(10))
+
+
+
+//Nums going DOWN
+for (let i = num; i > 0; i--) {
+    total += i
+}
+return total
 
 console.log(addition(10))
